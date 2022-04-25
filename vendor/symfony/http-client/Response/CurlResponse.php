19d6c7562a224ed94d7ff04a9cec4d5e27c78e50
Kevin <unforg.reuun@gmail.com>
2022-04-25 16:22:27 +0200

    modif

--- vendor/symfony/http-client/Response/CurlResponse.php
+++ vendor/symfony/http-client/Response/CurlResponse.php
@@ -257,7 +257,9 @@ final class CurlResponse implements ResponseInterface, StreamableInterface
 
             $this->doDestruct();
         } finally {
-            curl_setopt($this->handle, \CURLOPT_VERBOSE, false);
+            if (\is_resource($this->handle) || $this->handle instanceof \CurlHandle) {
+                curl_setopt($this->handle, \CURLOPT_VERBOSE, false);
+            }
         }
     }
 
@@ -404,6 +406,7 @@ final class CurlResponse implements ResponseInterface, StreamableInterface
                 } elseif (303 === $info['http_code'] || ('POST' === $info['http_method'] && \in_array($info['http_code'], [301, 302], true))) {
                     $info['http_method'] = 'HEAD' === $info['http_method'] ? 'HEAD' : 'GET';
                     curl_setopt($ch, \CURLOPT_POSTFIELDS, '');
+                    curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, $info['http_method']);
                 }
             }
 
