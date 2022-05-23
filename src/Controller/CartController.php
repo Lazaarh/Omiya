2281c6faf35aa2d1ce48ee67faefa9dd2c8ac013
Kevin <unforg.reuun@gmail.com>
2022-05-23 10:12:56 +0200

    modif cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -37,7 +37,7 @@ class CartController extends AbstractController
     {
         $cart->add($id);
         
-        return $this->redirectToRoute('cart');
+        return $this->redirectToRoute('products');
     }
 
 
